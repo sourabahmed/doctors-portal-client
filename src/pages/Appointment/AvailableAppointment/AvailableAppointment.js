d830c4de3a048ae899b90093e6ffe8c566d34710
unknown <sourabahmed>
2021-11-04 02:21:06 +0600

    modal-added

--- src/pages/Appointment/AvailableAppointment/AvailableAppointment.js
+++ src/pages/Appointment/AvailableAppointment/AvailableAppointment.js
@@ -1,10 +1,62 @@
+import { Grid, Container, Typography } from '@mui/material';
 import React from 'react';
+import Booking from '../Booking/Booking';
 
-const AvailableAppointment = ({date}) => {
+
+const bookings = [
+    {
+        id: 1,
+        name: 'Teeth Orthodonics',
+        time: '08.00 AM - 09.00 AM',
+        space: 10,
+    },
+    {
+        id: 2,
+        name: 'Cosmetcs Dentistry',
+        time: '09.00 AM - 10.00 AM',
+        space: 8,
+    },
+    {
+        id: 3,
+        name: 'Teeth Cleaning',
+        time: '10.00 AM - 11.00 AM',
+        space: 10,
+    },
+    {
+        id: 4,
+        name: 'Cavity Protection',
+        time: '11.00 AM - 12.00 PM',
+        space: 5,
+    },
+    {
+        id: 5,
+        name: 'Pediatric Dental',
+        time: '06.00 PM - 07.00 PM',
+        space: 10,
+    },
+    {
+        id: 6,
+        name: 'Oral Sergery',
+        time: '07.00 PM - 08.00 PM',
+        space: 9,
+    },
+]
+const AvailableAppointment = ({ date }) => {
     return (
-        <div>
-            <h2>Available appointment {date.toDateString()}</h2>
-        </div>
+        <Container>
+            <Typography sx={{ color: 'info.main', m:3 }} variant="h4" gutterBottom component="div">
+                Available Appointment on {date.toDateString()}
+            </Typography>
+            <Grid container spacing={2}>
+                {
+                    bookings.map(booking => <Booking
+                        key={booking.id}
+                        booking={booking}
+                        date={date}
+                    ></Booking>)
+                }
+            </Grid>
+        </Container>
     );
 };
 
---
+++ src/pages/Appointment/Booking/Booking.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Button, Grid } from '@mui/material';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import BookingModal from '../BookingModal/BookingModal';
+
+const Booking = ({ booking, date }) => {
+    const { name, time, space } = booking;
+    const [openBooking, setOpenBooking] = React.useState(false);
+    const handleBookingOpen = () => setOpenBooking(true);
+    const handleBookingClose = () => setOpenBooking(false);
+    return (
+        <>
+            <Grid item xs={12} sm={6} md={4}>
+                <Paper elevation={3} sx={{ py: 5 }}>
+                    <Typography sx={{ color: 'info.main', fontWeight: 600 }} variant="h5" gutterBottom component="div">
+                        {name}
+                    </Typography>
+                    <Typography variant="h6" gutterBottom component="div">
+                        {time}
+                    </Typography>
+                    <Typography variant="caption" display="block" gutterBottom>
+                        {space} SPACES AVAILABLE
+                    </Typography>
+                    <Button onClick={handleBookingOpen} variant="contained"> Book Appointment </Button>
+                </Paper>
+            </Grid >
+            <BookingModal 
+            booking={booking}
+            date={date}
+            handleBookingClose={handleBookingClose}
+            openBooking={openBooking}
+            ></BookingModal>
+        </>
+    );
+};
+
+export default Booking;
